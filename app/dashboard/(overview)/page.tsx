651501de77fbbf2e6d8d64346dd67ae8372c889f
kunal <kunalshinde0612@gmail.com>
2025-10-19 22:12:05 +0530

    commit

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -13,7 +13,6 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 export default async function Page() {
-  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
